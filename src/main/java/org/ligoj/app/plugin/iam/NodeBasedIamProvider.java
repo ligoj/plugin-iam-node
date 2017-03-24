d09ecefa3d6674c1a6117f67b8ac4dd637e88ea0
Fabrice Daugan <fabrice.daugan@gfi.fr>
2017-03-24 14:14:40 +0100

    Initialization

---
+++ src/main/java/org/ligoj/app/plugin/iam/NodeBasedIamProvider.java
@@ -0,0 +1,81 @@
+package org.ligoj.app.plugin.iam;
+
+import javax.cache.annotation.CacheResult;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ligoj.app.iam.IamConfiguration;
+import org.ligoj.app.iam.IamConfigurationProvider;
+import org.ligoj.app.iam.IamProvider;
+import org.ligoj.app.plugin.id.resource.IdentityServicePlugin;
+import org.ligoj.app.resource.ServicePluginLocator;
+import org.ligoj.bootstrap.resource.system.configuration.ConfigurationResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Identity and Access Management provider based on node. A primary node is used
+ * to fetch user details. The secondary provider can authenticate some users
+ * before the primary, when their login is accepted.
+ */
+@Component
+@Slf4j
+public class NodeBasedIamProvider implements IamProvider {
+
+	@Autowired
+	protected ServicePluginLocator servicePluginLocator;
+
+	@Autowired
+	protected ConfigurationResource configuration;
+
+	/**
+	 * Secondary user nodes.
+	 * 
+	 * @return Secondary user nodes. May be empty.
+	 */
+	protected String[] getSecondary() {
+		return StringUtils.defaultString(configuration.get("iam.secondary"), "").split(",");
+	}
+
+	/**
+	 * Secondary user nodes.
+	 * 
+	 * @return Secondary user nodes. Should not be <code>null</code>.
+	 */
+	protected String getPrimary() {
+		return configuration.get("iam.primary");
+	}
+
+	@Override
+	public Authentication authenticate(final Authentication authentication) {
+
+		// Determine the right provider to authenticate among the IAM nodes
+		for (final String nodeId : getSecondary()) {
+			final IdentityServicePlugin resource = servicePluginLocator.getResource(nodeId,
+					IdentityServicePlugin.class);
+			if (resource == null) {
+				// Ignore IAM provider not found
+				log.info("IAM node {} does not exist", nodeId);
+			} else if (resource.accept(authentication, nodeId)) {
+				// IAM provider has been found, use it for this authentication
+				return resource.authenticate(authentication, nodeId, false);
+			}
+		}
+
+		// Primary authentication
+		final String primary = getPrimary();
+		return servicePluginLocator.getResourceExpected(primary, IdentityServicePlugin.class)
+				.authenticate(authentication, primary, true);
+	}
+
+	@Override
+	@CacheResult(cacheName = "iam-node-configuration")
+	public IamConfiguration getConfiguration() {
+		final String primary = getPrimary();
+		return servicePluginLocator.getResourceExpected(primary, IamConfigurationProvider.class)
+				.getConfiguration(primary);
+	}
+
+}
---
+++ src/test/java/org/ligoj/app/plugin/iam/NodeBasedIamProviderTest.java
@@ -0,0 +1,123 @@
+package org.ligoj.app.plugin.iam;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.transaction.Transactional;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ligoj.app.iam.IamConfiguration;
+import org.ligoj.app.iam.IamConfigurationProvider;
+import org.ligoj.app.model.DelegateNode;
+import org.ligoj.app.model.Event;
+import org.ligoj.app.model.Node;
+import org.ligoj.app.model.Parameter;
+import org.ligoj.app.model.ParameterValue;
+import org.ligoj.app.model.Project;
+import org.ligoj.app.model.Subscription;
+import org.ligoj.app.plugin.id.resource.IdentityServicePlugin;
+import org.ligoj.app.resource.AbstractServerTest;
+import org.ligoj.app.resource.ServicePluginLocator;
+import org.ligoj.bootstrap.model.system.SystemConfiguration;
+import org.ligoj.bootstrap.resource.system.configuration.ConfigurationResource;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Test class of {@link NodeBasedIamProvider}
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:/META-INF/spring/application-context-test.xml")
+@Rollback
+@Transactional
+public class NodeBasedIamProviderTest extends AbstractServerTest {
+
+	@Autowired
+	private ConfigurationResource configuration;
+
+	@Before
+	public void prepareSubscription() throws IOException {
+		persistEntities(
+				"csv/app-test", new Class[] { SystemConfiguration.class, Node.class, Parameter.class, Project.class,
+						Subscription.class, ParameterValue.class, Event.class, DelegateNode.class },
+				StandardCharsets.UTF_8.name());
+	}
+
+	@Test
+	public void authenticateNoSecondary() throws Exception {
+		final Authentication authentication = new UsernamePasswordAuthenticationToken("user1", "secret");
+		final Authentication authentication2 = new UsernamePasswordAuthenticationToken("user1v2", "secret");
+		final NodeBasedIamProvider provider = new NodeBasedIamProvider();
+		final IdentityServicePlugin servicePlugin = Mockito.mock(IdentityServicePlugin.class);
+		provider.configuration = configuration;
+		provider.servicePluginLocator = Mockito.mock(ServicePluginLocator.class);
+		Mockito.when(
+				provider.servicePluginLocator.getResourceExpected("service:id:ldap:dig", IdentityServicePlugin.class))
+				.thenReturn(servicePlugin);
+		Mockito.when(servicePlugin.authenticate(authentication, "service:id:ldap:dig", true))
+				.thenReturn(authentication2);
+		Assert.assertSame(authentication2, provider.authenticate(authentication));
+	}
+
+	@Test
+	public void authenticateSecondaryAccept() throws Exception {
+		final Authentication authentication = new UsernamePasswordAuthenticationToken("user1", "secret");
+		final Authentication authentication2 = new UsernamePasswordAuthenticationToken("user1v2", "secret");
+		final IdentityServicePlugin servicePlugin = Mockito.mock(IdentityServicePlugin.class);
+		final NodeBasedIamProvider provider = new NodeBasedIamProvider();
+		provider.configuration = configuration;
+		provider.servicePluginLocator = Mockito.mock(ServicePluginLocator.class);
+		Mockito.when(provider.servicePluginLocator.getResource("service:id:ldap:adu", IdentityServicePlugin.class))
+				.thenReturn(servicePlugin);
+		Mockito.when(servicePlugin.accept(authentication, "service:id:ldap:adu")).thenReturn(true);
+		Mockito.when(servicePlugin.authenticate(authentication, "service:id:ldap:adu", false))
+				.thenReturn(authentication2);
+		Assert.assertSame(authentication2, provider.authenticate(authentication));
+		Mockito.verify(provider.servicePluginLocator, VerificationModeFactory.times(0))
+				.getResource("service:id:ldap:dig", IdentityServicePlugin.class);
+	}
+
+	@Test
+	public void authenticateSecondaryDontAccept() throws Exception {
+		final Authentication authentication = new UsernamePasswordAuthenticationToken("user1", "secret");
+		final Authentication authentication2 = new UsernamePasswordAuthenticationToken("user1v2", "secret");
+		final IdentityServicePlugin servicePluginSecondary = Mockito.mock(IdentityServicePlugin.class);
+		final IdentityServicePlugin servicePluginPrimary = Mockito.mock(IdentityServicePlugin.class);
+		final NodeBasedIamProvider provider = new NodeBasedIamProvider();
+		provider.configuration = configuration;
+		provider.servicePluginLocator = Mockito.mock(ServicePluginLocator.class);
+		Mockito.when(provider.servicePluginLocator.getResource("service:id:ldap:adu", IdentityServicePlugin.class))
+				.thenReturn(servicePluginSecondary);
+		Mockito.when(servicePluginPrimary.authenticate(authentication, "service:id:ldap:dig", true))
+				.thenReturn(authentication2);
+		Mockito.when(
+				provider.servicePluginLocator.getResourceExpected("service:id:ldap:dig", IdentityServicePlugin.class))
+				.thenReturn(servicePluginPrimary);
+		Assert.assertSame(authentication2, provider.authenticate(authentication));
+		Mockito.verify(servicePluginSecondary, VerificationModeFactory.times(0)).authenticate(authentication,
+				"service:id:ldap:adu", false);
+	}
+
+	@Test
+	public void getConfiguration() {
+		final NodeBasedIamProvider provider = new NodeBasedIamProvider();
+		provider.configuration = configuration;
+		provider.servicePluginLocator = Mockito.mock(ServicePluginLocator.class);
+		final IamConfiguration iamConfiguration = Mockito.mock(IamConfiguration.class);
+		final IamConfigurationProvider servicePlugin = Mockito.mock(IamConfigurationProvider.class);
+		Mockito.when(servicePlugin.getConfiguration("service:id:ldap:dig")).thenReturn(iamConfiguration);
+		Mockito.when(provider.servicePluginLocator.getResourceExpected("service:id:ldap:dig",
+				IamConfigurationProvider.class)).thenReturn(servicePlugin);
+
+		Assert.assertSame(iamConfiguration, provider.getConfiguration());
+	}
+}
