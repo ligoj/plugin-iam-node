d2b34c8fb7c30735616461a51ed6e324fc12713c
Fabrice Daugan <fabrice.daugan@gmail.com>
2017-04-05 18:21:41 +0200

    Prepare 0.0.2 dev

--- travis.sh
+++ travis.sh
@@ -127,7 +127,7 @@ BUILD)
 
     mvn clean package jacoco:report sonar:sonar \
           $MAVEN_ARGS \
-          -Pjacoco -Djacoco.includes=org.ligoj.app.plugin.iam.node.* \
+          -Pjacoco -Djacoco.includes=org.ligoj.app.plugin.iam.* \
           -Dsonar.host.url=$SONAR_HOST_URL \
           -Dsonar.organization=ligoj-github \
           -Dsonar.login=$SONAR_TOKEN \
